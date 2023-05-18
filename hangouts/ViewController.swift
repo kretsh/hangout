c99fa85c14bbbc5c87e95f5a4d31c6765d68adaf
Clorinda Owen <cowen@c1s8d37.42wolfsburg.de>
2023-05-18 13:38:59 +0200

    init commit

---
+++ hangouts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hangouts
+//
+//  Created by Clorinda Owen on 5/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
